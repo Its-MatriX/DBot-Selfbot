aab48bead83a06f596fa16435026309a674e6d28
Its-MatriX <109545908+Its-MatriX@users.noreply.github.com>
2022-09-18 15:06:40 +0300

    Delete logger.py

--- logger.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from colorama import Fore
-
-
-def log(content, message_type='ИНФО   ', color=Fore.YELLOW):
-    time_now = datetime.now().strftime('%H:%M:%S')
-    print(
-        f'{Fore.CYAN}{time_now}{Fore.WHITE} {color}[{message_type}]{Fore.WHITE} -> {Fore.GREEN}{content}{Fore.WHITE}'
-    )

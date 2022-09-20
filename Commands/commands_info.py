76bfd9384990ec2d4cf9f7430b6a63d915b1a497
Its-MatriX <109545908+Its-MatriX@users.noreply.github.com>
2022-09-20 22:27:33 +0300

    Add files via upload

--- Commands/commands_info.py
+++ Commands/commands_info.py
@@ -1,6 +1,6 @@
 from discord import Status, User
 from discord.ext import commands
-
+import requests
 
 class InfoCog(commands.Cog):
 
@@ -39,23 +39,26 @@ class InfoCog(commands.Cog):
 
         resp = ''
 
+        url = 'http://tinyurl.com/api-create.php?url=' + str(user.avatar_url)
+        avatar_url = requests.get(url).text
+
         if global_user:
-            resp += f'**Имя пользователя:** `{user}`\n'
-            resp += f'**ID:** `{user.id}`\n'
-            resp += f'**Бот:** `{"Да" if user.bot else "Нет"}`\n'
-            resp += f'**Друг:** `{"Да" if user.is_friend() else "Нет"}`\n'
-            resp += f'**Ссылка на аватарку:** `{user.avatar_url}`\n'
+            resp += f'> **Имя пользователя:** `{user}`\n'
+            resp += f'> **ID:** `{user.id}`\n'
+            resp += f'> **Бот:** `{"Да" if user.bot else "Нет"}`\n'
+            resp += f'> **Друг:** `{"Да" if user.is_friend() else "Нет"}`\n'
+            resp += f'> **Аватар:** **{avatar_url}**\n'
             created_at = round(user.created_at.timestamp())
-            resp += f'**Создан: <t:{created_at}> - <t:{created_at}:R>**'
+            resp += f'> **Создано:** **<t:{created_at}:R>**'
 
         else:
-            resp += f'**Имя пользователя:** `{user}`\n'
-            resp += f'**Имя на сервере:** `{user.display_name}`\n'
-            resp += f'**ID:** `{user.id}`\n'
-            resp += f'**Бот:** `{"Да" if user.bot else "Нет"}`\n'
-            resp += f'**Ссылка на аватарку:** `{user.avatar_url}`\n'
+            resp += f'> **Имя пользователя:** `{user}`\n'
+            resp += f'> **Имя на сервере:** `{user.display_name}`\n'
+            resp += f'> **ID:** `{user.id}`\n'
+            resp += f'> **Бот:** `{"Да" if user.bot else "Нет"}`\n'
+            resp += f'> **Аватар:** **{avatar_url}**\n'
             created_at = round(user.created_at.timestamp())
-            resp += f'**Создан: <t:{created_at}> - <t:{created_at}:R>**\n'
+            resp += f'> **Создано:** **<t:{created_at}:R>**\n'
 
             if user.status == Status.online:
                 status_icon = 'В сети'
@@ -72,14 +75,14 @@ class InfoCog(commands.Cog):
             if user.is_on_mobile():
                 status_icon += ' (телефон)'
 
-            resp += f'**Статус:** `{status_icon}`\n'
+            resp += f'> **Статус:** **{status_icon}**\n'
 
-            resp += f'**Высшая роль:** `{user.top_role.name}`\n'
-            resp += f'**Имеет ролей:** `{len(user.roles)}`\n'
-            resp += f'**Администратор:** `{"Да" if user.guild_permissions.administrator else "Нет"}`\n'
+            resp += f'> **Высшая роль:** `{user.top_role.name}`\n'
+            resp += f'> **Имеет ролей:** `{len(user.roles)}`\n'
+            resp += f'> **Администратор:** `{"Да" if user.guild_permissions.administrator else "Нет"}`\n'
 
             if user.guild.owner:
-                resp += f'**Владелец:** `{"Да" if user.id == user.guild.owner.id else "Нет"}`'
+                resp += f'> **Владелец:** `{"Да" if user.id == user.guild.owner.id else "Нет"}`'
 
         await ctx.send(resp)
 
@@ -97,19 +100,19 @@ class InfoCog(commands.Cog):
 
         afk_channel = ctx.guild.afk_channel if ctx.guild.afk_channel else 'Нет'
 
-        resp += f'**Название:** `{ctx.guild.name}`\n'
-        resp += f'**ID:** `{ctx.guild.id}`\n'
+        resp += f'> **Название:** `{ctx.guild.name}`\n'
+        resp += f'> **ID:** `{ctx.guild.id}`\n'
 
         try:
-            resp += f'**AFK канал:** {afk_channel.mention}\n'
-            resp += f'**AFK таймаут:** `{ctx.guild.afk_timeout} секунд`\n'
+            resp += f'> **AFK канал:** {afk_channel.mention}\n'
+            resp += f'> **AFK таймаут:** `{ctx.guild.afk_timeout} секунд`\n'
         except:
-            resp += f'**AFK канал:** `Нет`\n'
+            resp += f'> **AFK канал:** `Нет`\n'
 
         try:
-            resp += f'**Канал с правилами:** {ctx.guild.rules_channel.mention}\n'
+            resp += f'> **Канал с правилами:** {ctx.guild.rules_channel.mention}\n'
         except:
-            resp += f'**Канал с правилами:** `Нет`\n'
+            resp += f'> **Канал с правилами:** `Нет`\n'
 
         verification = str(ctx.guild.verification_level)
 
@@ -124,30 +127,39 @@ class InfoCog(commands.Cog):
         elif verification == 'extreme':
             verification = 'Самый высокий'
 
-        resp += f'**Уровень верификации:** `{verification}`\n'
+        resp += f'> **Уровень верификации:** `{verification}`\n'
 
         roles = len(ctx.guild.roles)
         admin_roles = len(
             [x for x in ctx.guild.roles if x.permissions.administrator])
 
-        resp += f'**Роли:** `{roles}, администраторских: {admin_roles}`\n'
+        resp += f'> **Роли:** `{roles}, администраторских: {admin_roles}`\n'
 
         channels = len(ctx.guild.channels)
         text_channels = len(ctx.guild.text_channels)
         voice_channels = len(ctx.guild.voice_channels)
         cateories = len(ctx.guild.categories)
 
-        resp += f'**Каналы:** `{channels} (текстовых: {text_channels}, голосовых: {voice_channels}, категорий: {cateories})`\n'
+        resp += f'> **Каналы:** `{channels} (текстовых: {text_channels}, голосовых: {voice_channels}, категорий: {cateories})`\n'
 
-        resp += f'**Описание:** `{ctx.guild.description}`\n'
-        resp += f'**Ссылка на аватарку:** `{ctx.guild.icon_url}`\n'
+        resp += f'> **Описание:** `{ctx.guild.description}`\n'
+
+        url = 'http://tinyurl.com/api-create.php?url=' + str(ctx.guild.icon_url)
+        icon_url = requests.get(url).text
+    
+        resp += f'> **Иконка:** **{icon_url}**\n'
 
         try:
-            resp += f'**Ссылка на баннер:** `{ctx.guild.banner.url}`\n'
+            url = 'http://tinyurl.com/api-create.php?url=' + str(ctx.guild.banner.url)
+            banner_url = requests.get(url).text
+            resp += f'> **Баннер:** **{banner_url}**\n'
         except:
             pass
 
-        resp += f'**Ссылка на виджет:** `https://discord.com/widget?id={ctx.guild.id}`\n'
+        url = 'http://tinyurl.com/api-create.php?url=' + f'https://discord.com/widget?id={ctx.guild.id}'
+        widget_url = requests.get(url).text
+
+        resp += f'> **Виджет:** **{widget_url}**\n'
 
         await ctx.send(resp)
 
@@ -160,7 +172,7 @@ class InfoCog(commands.Cog):
 
         ping = round(self.bot.latency * 1000)
 
-        resp = f'**Пинг:** `{ping} МС`'
+        resp = f'> **Пинг:** `{ping} МС`'
         await ctx.send(resp)
 
 

3cd8362db8b0f0699009c1f5c9375a5d93446f43
Its-MatriX <109545908+Its-MatriX@users.noreply.github.com>
2022-09-21 12:12:07 +0300

    Add files via upload

--- intro.py
+++ intro.py
@@ -1,7 +1,6 @@
 from colorama import Fore
 from Commands.colors import colors_text_v2, gradient_vertical
 
-
 intro_text = '''┏━━━┳┓    ┏┓
 ┗┓┏┓┃┃   ┏┛┗┓
  ┃┃┃┃┗━┳━┻┓┏┛
@@ -9,6 +8,7 @@ intro_text = '''┏━━━┳┓    ┏┓
 ┏┛┗┛┃┗┛┃┗┛┃┗┓
 ┗━━━┻━━┻━━┻━┛
 '''
+
 
 def intro():
     print(gradient_vertical(intro_text, colors_text_v2))

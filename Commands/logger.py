1bf0365fd4b6bc1b3b5ad3d233fd3dcc39e271d5
Its-MatriX <109545908+Its-MatriX@users.noreply.github.com>
2022-09-24 23:41:01 +0300

    Add files via upload

--- Commands/logger.py
+++ Commands/logger.py
@@ -8,9 +8,12 @@ class logwriter:
     written = ''
 
 
-def log(content, message_type='ИНФО', spaces=3):
-    time_now = datetime.now().strftime('%H:%M:%S')
-    to_write = f'{gradient_horizontal(time_now, colors_text)} {gradient_horizontal("[" + message_type + "]", colors_text_v2)}{" " * spaces} : {gradient_horizontal(content, colors_text)}'
+def log(content, message_type='ИНФО', spaces=3, show_type=True):
+    if show_type:
+        time_now = datetime.now().strftime('%H:%M:%S')
+        to_write = f'{gradient_horizontal(time_now, colors_text)} {gradient_horizontal("[" + message_type + "]", colors_text_v2)}{" " * spaces} : {gradient_horizontal(content, colors_text)}'
+    else:
+        to_write = gradient_horizontal(content, colors_text)
 
     print(to_write)
 
@@ -21,9 +24,12 @@ def log(content, message_type='ИНФО', spaces=3):
         logwriter.written = '\n'.join(logwriter.written[:50])
 
 
-def log_error(content, message_type='ОШИБКА', spaces=1):
-    time_now = datetime.now().strftime('%H:%M:%S')
-    to_write = f'{gradient_horizontal(time_now, colors_text)} {gradient_horizontal("[" + message_type + "]", colors_error)}{" " * spaces} : {gradient_horizontal(content, colors_text)}'
+def log_error(content, message_type='ОШИБКА', spaces=1, show_type=True):
+    if show_type:
+        time_now = datetime.now().strftime('%H:%M:%S')
+        to_write = f'{gradient_horizontal(time_now, colors_text)} {gradient_horizontal("[" + message_type + "]", colors_error)}{" " * spaces} : {gradient_horizontal(content, colors_text)}'
+    else:
+        to_write = gradient_horizontal(content, colors_text)
 
     print(to_write)
 

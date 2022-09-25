63dbf545fef1a8e08fcc08b9e0b5e0ccb054d90f
Its-MatriX <109545908+Its-MatriX@users.noreply.github.com>
2022-09-25 17:12:42 +0300

    Add files via upload

---
+++ Functions/intro.py
@@ -0,0 +1,16 @@
+from Functions.colors import colors_text_v2, gradient_vertical
+
+# Some remake logo and
+# fix display on Pydroid 3 (for android)
+
+intro_text = '''╔═══╦╗    ╔╗  
+╚╗╔╗║║   ╔╝╚╗  
+ ║║║║╚═╦═╩╗╔╝  
+ ║║║║╔╗║╔╗║║  
+╔╝╚╝║╚╝║╚╝║╚╗  
+╚═══╩══╩══╩═╝  
+'''
+
+
+def intro():
+    print(gradient_vertical(intro_text, colors_text_v2))

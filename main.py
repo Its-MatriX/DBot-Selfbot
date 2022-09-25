2ddbd76ea837a8ff61ba3f4d6dddc801997c7f71
Its-MatriX <109545908+Its-MatriX@users.noreply.github.com>
2022-09-25 13:19:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,42 +3,16 @@
 
 from os.path import sep, split, isfile
 from os import system
-from sys import executable
 
 folder = split(__file__)[0]
 
-required = open(folder + sep + 'requirements.txt', 'r').read().split('\n')
-
-from subprocess import run
-
-installed = run([executable, '-m', 'pip', 'freeze'],
-                capture_output=True,
-                text=True).stdout
-
-installed = installed.split('\n')
-installed_ = []
-
-for module in installed:
-    installed_.append(module.split('==')[0].lower())
-
-installed = installed_
-install_modules = False
-
-for req in required:
-    if req.lower() not in installed:
-        print('У вас не установлены все необходимые модули!')
-        input('Нажмите [Enter] для начала установки\n> ')
-        install_modules = True
-        break
-
-if install_modules:
-    for req in required:
-        system(f'{executable} -m pip install {req}')
-
-from json import load
-from os import get_terminal_size, listdir, name
-from discord import Activity, ActivityType, Status, Streaming, Game
-from requests import get
+try:
+    from json import load
+    from os import get_terminal_size, listdir, name
+    from discord import Activity, ActivityType, Status, Streaming, Game
+    from requests import get
+except:
+    import Commands.requirements_installer
 
 if isfile(folder + sep + 'INDICATOR_UPDATE_INSTALL.txt'):
     import updater
@@ -50,16 +24,19 @@ clear = lambda: system('cls') if name == 'nt' else system('clear')
 
 clear()
 
-from threading import Thread
-from time import sleep as non_async_sleep
+try:
+    from threading import Thread
+    from time import sleep as non_async_sleep
 
-from colorama import Fore
-from discord.ext import commands
+    from colorama import Fore
+    from discord.ext import commands
 
-from Commands.colors import (colors_text, colors_text_v2, gradient_horizontal,
-                             print_line)
-from Commands.logger import log, log_error, recovery_logs, logwriter
-from intro import intro
+    from Commands.colors import (colors_text, colors_text_v2, gradient_horizontal,
+                                print_line)
+    from Commands.logger import log, log_error, recovery_logs, logwriter
+    from intro import intro
+except:
+    import Commands.requirements_installer
 
 loaded_extensions = 0
 

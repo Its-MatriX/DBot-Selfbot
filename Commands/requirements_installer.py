d02b09b829a20f26712e3006932b086e727fbd51
Its-MatriX <109545908+Its-MatriX@users.noreply.github.com>
2022-09-25 13:17:11 +0300

    Add files via upload

---
+++ Commands/requirements_installer.py
@@ -0,0 +1,33 @@
+from os.path import sep, split
+from os import system
+from sys import executable
+
+folder = split(__file__)[0]
+
+required = open(folder + sep + 'requirements.txt', 'r').read().split('\n')
+
+from subprocess import run
+
+installed = run([executable, '-m', 'pip', 'freeze'],
+                capture_output=True,
+                text=True).stdout
+
+installed = installed.split('\n')
+installed_ = []
+
+for module in installed:
+    installed_.append(module.split('==')[0].lower())
+
+installed = installed_
+install_modules = False
+
+for req in required:
+    if req.lower() not in installed:
+        print('У вас не установлены все необходимые модули!')
+        input('Нажмите [Enter] для начала установки\n> ')
+        install_modules = True
+        break
+
+if install_modules:
+    for req in required:
+        system(f'{executable} -m pip install {req}')

ddb031e6de49afce0c9c74726a2e67d5cd723947
Its-MatriX <109545908+Its-MatriX@users.noreply.github.com>
2022-10-01 17:08:18 +0300

    Update intro.py

--- Functions/intro.py
+++ Functions/intro.py
@@ -1,8 +1,41 @@
-from colorama import Fore
+intro_text = '''▓█████▄  ▄▄▄▄    ▒█████  ▄▄▄█████▓
+▒██▀ ██▌▓█████▄ ▒██▒  ██▒▓  ██▒ ▓▒
+░██   █▌▒██▒ ▄██▒██░  ██▒▒ ▓██░ ▒░
+░▓█▄   ▌▒██░█▀  ▒██   ██░░ ▓██▓ ░
+░▒████▓ ░▓█  ▀█▓░ ████▓▒░  ▒██▒ ░
+ ▒▒▓  ▒ ░▒▓███▀▒░ ▒░▒░▒░   ▒ ░░
+ ░ ▒  ▒ ▒░▒   ░   ░ ▒ ▒░     ░
+ ░ ░  ░  ░    ░ ░ ░ ░ ▒    ░
+   ░     ░          ░ ░
+ ░            ░
+ '''
 
-intro_text = '''Подождите, интро скоро будет
-'''
 
+def fore(color):
+    return '\033[38;5;%dm' % color
+
+
+def back(color):
+    return '\033[48;5;%dm' % color
+
+
+def get_color(id, mode):
+    if mode == 'bg':
+        return back(id)
+
+    elif mode == 'fg':
+        return fore(id)
+
+
+colors = [
+    93, 99, 105, 111, 117, 123, 122, 121, 120, 119, 118, 119, 120, 121, 122,
+    123, 117, 111, 105, 99, 93
+]
 
 def intro():
-    print(Fore.CYAN + intro_text)
+    lines = intro_text.split('\n')
+    index = 0
+
+    for line in lines:
+        print(get_color(colors[index], 'fg') + line)
+        index += 1

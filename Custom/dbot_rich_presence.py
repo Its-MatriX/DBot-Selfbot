8225aa356d369865fd33fdc5890eb6c6df1675f7
Its-MatriX <109545908+Its-MatriX@users.noreply.github.com>
2022-10-09 01:12:48 +0300

    Add files via upload

--- Custom/dbot_rich_presence.py
+++ Custom/dbot_rich_presence.py
@@ -6,28 +6,32 @@ from asyncio import new_event_loop, set_event_loop
 
 
 def start_rich_presence():
-    loop = new_event_loop()
-    set_event_loop(loop)
+    try:
+        loop = new_event_loop()
+        set_event_loop(loop)
 
-    client_id = "1023113143486005319"
-    RPC = Presence(client_id)
+        client_id = "1023113143486005319"
+        RPC = Presence(client_id)
 
-    RPC.connect()
-    RPC.update(details='DBot - Discord Selfbot',
-               state='Удобный, многофункциональный.',
-               start=time.time(),
-               large_image='icon',
-               large_text='DBot Icon',
-               buttons=[{
-                   'label': 'Скачать DBot',
-                   'url': 'https://github.com/Its-MatriX/DBot-Selfbot'
-               }, {
-                   'label': 'Сервер',
-                   'url': 'https://discord.gg/EC4tDfQYwf'
-               }])
+        RPC.connect()
+        RPC.update(details='DBot - Discord Selfbot',
+                   state='Удобный, многофункциональный.',
+                   start=time.time(),
+                   large_image='icon',
+                   large_text='DBot Icon',
+                   buttons=[{
+                       'label': 'Скачать DBot',
+                       'url': 'https://github.com/Its-MatriX/DBot-Selfbot'
+                   }, {
+                       'label': 'Сервер',
+                       'url': 'https://discord.gg/EC4tDfQYwf'
+                   }])
 
-    while True:
-        time.sleep(15)
+        while True:
+            time.sleep(15)
+
+    except:
+        pass
 
 
 class RichPresenceCog(commands.Cog):

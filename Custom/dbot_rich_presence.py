5c70940cabf9c8e07790b7b65e30d1aff64ff904
Its-MatriX <109545908+Its-MatriX@users.noreply.github.com>
2022-10-11 19:52:53 +0300

    Update dbot_rich_presence.py

--- Custom/dbot_rich_presence.py
+++ Custom/dbot_rich_presence.py
@@ -14,18 +14,24 @@ def start_rich_presence():
         RPC = Presence(client_id)
 
         RPC.connect()
-        RPC.update(details='DBot - Discord Selfbot',
-                   state='Удобный, многофункциональный.',
-                   start=time.time(),
-                   large_image='icon',
-                   large_text='DBot Icon',
-                   buttons=[{
-                       'label': 'Скачать DBot',
-                       'url': 'https://github.com/Its-MatriX/DBot-Selfbot'
-                   }, {
-                       'label': 'Сервер',
-                       'url': 'https://discord.gg/EC4tDfQYwf'
-                   }])
+        RPC.update(
+                    details='DBot - Discord Selfbot',
+                    state='Удобный, многофункциональный.',
+                    start=time.time(),
+                    large_image='icon',
+                    large_text='DBot Icon',
+                    buttons=[
+                            {
+                                'label': 'Скачать DBot',
+                                'url': 'https://github.com/Its-MatriX/DBot-Selfbot'
+                            },
+                        
+                            {
+                                'label': 'Сервер',
+                                'url': 'https://discord.gg/EC4tDfQYwf'
+                            }
+                        ]
+                    )
 
         while True:
             time.sleep(15)
@@ -38,7 +44,10 @@ class RichPresenceCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        Thread(target=start_rich_presence).start()
+
+        Thread(
+                target=start_rich_presence
+            ).start()
 
 
 def setup(bot):

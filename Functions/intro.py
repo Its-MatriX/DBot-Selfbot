4ec1e82102716681517f0e0f684a7177a5684b9c
Its-MatriX <109545908+Its-MatriX@users.noreply.github.com>
2022-11-13 23:59:00 +0300

    Add files via upload

--- Functions/intro.py
+++ Functions/intro.py
@@ -1,4 +1,24 @@
-intro_text = ''' _____  ____        _      _____      _  __ _           _   
+# Note: "theme.py"
+# Цвет заголовка (HEADER_COLOR)
+# 1 - Стандартный (градиент)
+# 2 - Синий
+# 3 - Голубой
+# 4 - Зелёный
+# 5 - Фиолетовый
+# 6 - Жёлтый
+
+# Стиль заголовка (HEADER_STYLE)
+# 1 - Стандартный
+# 2 - Bloody
+# 3 - Colossal
+# 4 - DOS Rebel
+
+from theme import LOGO_COLOR, LOGO_STYLE
+from colorama import Fore
+from Functions.logger import log_error
+from os import _exit
+
+IntroText_Default = ''' _____  ____        _      _____      _  __ _           _   
 |  __ \|  _ \      | |    / ____|    | |/ _| |         | |
 | |  | | |_) | ___ | |_  | (___   ___| | |_| |__   ___ | | 
 | |  | |  _ < / _ \| __|  \___ \ / _ \ |  _| '_ \ / _ \| __|
@@ -6,12 +26,76 @@ intro_text = ''' _____  ____        _      _____      _  __ _           _
 |_____/|____/ \___/ \__| |_____/ \___|_|_| |_.__/ \___/ \__|
 '''
 
-intro_text_mini = ''' _____  ____        _ 
+IntroText_Default_Mini = ''' _____  ____        _ 
 |  __ \|  _ \      | |
 | |  | | |_) | ___ | |
 | |  | |  _ < / _ \| __|
 | |__| | |_) | (_) | |_
 |_____/|____/ \___/ \__|
+'''
+
+IntroText_Bloody = '''▓█████▄  ▄▄▄▄    ▒█████  ▄▄▄█████▓     ██████ ▓█████  ██▓      █████▒▄▄▄▄    ▒█████  ▄▄▄█████▓
+▒██▀ ██▌▓█████▄ ▒██▒  ██▒▓  ██▒ ▓▒   ▒██    ▒ ▓█   ▀ ▓██▒    ▓██   ▒▓█████▄ ▒██▒  ██▒▓  ██▒ ▓▒
+░██   █▌▒██▒ ▄██▒██░  ██▒▒ ▓██░ ▒░   ░ ▓██▄   ▒███   ▒██░    ▒████ ░▒██▒ ▄██▒██░  ██▒▒ ▓██░ ▒░
+░▓█▄   ▌▒██░█▀  ▒██   ██░░ ▓██▓ ░      ▒   ██▒▒▓█  ▄ ▒██░    ░▓█▒  ░▒██░█▀  ▒██   ██░░ ▓██▓ ░ 
+░▒████▓ ░▓█  ▀█▓░ ████▓▒░  ▒██▒ ░    ▒██████▒▒░▒████▒░██████▒░▒█░   ░▓█  ▀█▓░ ████▓▒░  ▒██▒ ░ 
+ ▒▒▓  ▒ ░▒▓███▀▒░ ▒░▒░▒░   ▒ ░░      ▒ ▒▓▒ ▒ ░░░ ▒░ ░░ ▒░▓  ░ ▒ ░   ░▒▓███▀▒░ ▒░▒░▒░   ▒ ░░   
+ ░ ▒  ▒ ▒░▒   ░   ░ ▒ ▒░     ░       ░ ░▒  ░ ░ ░ ░  ░░ ░ ▒  ░ ░     ▒░▒   ░   ░ ▒ ▒░     ░    
+ ░ ░  ░  ░    ░ ░ ░ ░ ▒    ░         ░  ░  ░     ░     ░ ░    ░ ░    ░    ░ ░ ░ ░ ▒    ░      
+   ░     ░          ░ ░                    ░     ░  ░    ░  ░        ░          ░ ░           
+ ░            ░                                                           ░                   
+ '''
+
+IntroText_Bloody_Mini = '''▓█████▄  ▄▄▄▄    ▒█████  ▄▄▄█████▓
+▒██▀ ██▌▓█████▄ ▒██▒  ██▒▓  ██▒ ▓▒
+░██   █▌▒██▒ ▄██▒██░  ██▒▒ ▓██░ ▒░
+░▓█▄   ▌▒██░█▀  ▒██   ██░░ ▓██▓ ░
+░▒████▓ ░▓█  ▀█▓░ ████▓▒░  ▒██▒ ░
+ ▒▒▓  ▒ ░▒▓███▀▒░ ▒░▒░▒░   ▒ ░░
+ ░ ▒  ▒ ▒░▒   ░   ░ ▒ ▒░     ░ 
+ ░ ░  ░  ░    ░ ░ ░ ░ ▒    ░    
+   ░     ░          ░ ░        
+ ░            ░              
+ '''
+
+IntroText_Colossal = '''8888888b.  888888b.            888          .d8888b.           888  .d888 888               888    
+888  "Y88b 888  "88b           888         d88P  Y88b          888 d88P"  888               888    
+888    888 888  .88P           888         Y88b.               888 888    888               888    
+888    888 8888888K.   .d88b.  888888       "Y888b.    .d88b.  888 888888 88888b.   .d88b.  888888 
+888    888 888  "Y88b d88""88b 888             "Y88b. d8P  Y8b 888 888    888 "88b d88""88b 888    
+888    888 888    888 888  888 888               "888 88888888 888 888    888  888 888  888 888    
+888  .d88P 888   d88P Y88..88P Y88b.       Y88b  d88P Y8b.     888 888    888 d88P Y88..88P Y88b.  
+8888888P"  8888888P"   "Y88P"   "Y888       "Y8888P"   "Y8888  888 888    88888P"   "Y88P"   "Y888 
+'''
+
+IntroText_Colossal_Mini = '''8888888b.  888888b.            888
+888  "Y88b 888  "88b           888
+888    888 888  .88P           888
+888    888 8888888K.   .d88b.  888888
+888    888 888  "Y88b d88""88b 888
+888    888 888    888 888  888 888
+888  .d88P 888   d88P Y88..88P Y88b.
+8888888P"  8888888P"   "Y88P"   "Y888
+'''
+
+IntroText_DOSRebel = ''' ██████████   ███████████            █████        █████████           ████     ██████  █████               █████   
+░░███░░░░███ ░░███░░░░░███          ░░███        ███░░░░░███         ░░███    ███░░███░░███               ░░███    
+ ░███   ░░███ ░███    ░███  ██████  ███████     ░███    ░░░   ██████  ░███   ░███ ░░░  ░███████   ██████  ███████  
+ ░███    ░███ ░██████████  ███░░███░░░███░      ░░█████████  ███░░███ ░███  ███████    ░███░░███ ███░░███░░░███░   
+ ░███    ░███ ░███░░░░░███░███ ░███  ░███        ░░░░░░░░███░███████  ░███ ░░░███░     ░███ ░███░███ ░███  ░███    
+ ░███    ███  ░███    ░███░███ ░███  ░███ ███    ███    ░███░███░░░   ░███   ░███      ░███ ░███░███ ░███  ░███ ███
+ ██████████   ███████████ ░░██████   ░░█████    ░░█████████ ░░██████  █████  █████     ████████ ░░██████   ░░█████ 
+░░░░░░░░░░   ░░░░░░░░░░░   ░░░░░░     ░░░░░      ░░░░░░░░░   ░░░░░░  ░░░░░  ░░░░░     ░░░░░░░░   ░░░░░░     ░░░░░  
+'''
+
+IntroText_DOSRebel_Mini = ''' ██████████   ███████████            █████
+░░███░░░░███ ░░███░░░░░███          ░░███
+ ░███   ░░███ ░███    ░███  ██████  ███████
+ ░███    ░███ ░██████████  ███░░███░░░███░
+ ░███    ░███ ░███░░░░░███░███ ░███  ░███
+ ░███    ███  ░███    ░███░███ ░███  ░███ ███
+ ██████████   ███████████ ░░██████   ░░█████
+░░░░░░░░░░   ░░░░░░░░░░░   ░░░░░░     ░░░░░
 '''
 
 
@@ -37,20 +121,131 @@ colors = [
 ]
 
 
-def intro(mini = False):
-
+def intro(mini=False):
     if not mini:
-        lines = intro_text.split('\n')
+        if LOGO_STYLE == 1:
+            lines = IntroText_Default.split('\n')
+
+        elif LOGO_STYLE == 2:
+            lines = IntroText_Bloody.split('\n')
+
+        elif LOGO_STYLE == 3:
+            lines = IntroText_Colossal.split('\n')
+
+        elif LOGO_STYLE == 4:
+            lines = IntroText_DOSRebel.split('\n')
+
+        else:
+            log_error('Неверное значение для HEADER_STYLE в "theme.py", ' + \
+                              'значение должно быть в диапазоне от 1 до 4.')
+
+            try:
+                input()
+
+            except:
+                pass
+
+            _exit(1)
+
         index = 0
 
         for line in lines:
-            print(get_color(colors[index], 'fg') + line)
+            if LOGO_COLOR == 1:
+                print(get_color(colors[index], 'fg') + line)
+
+            else:
+                if LOGO_COLOR == 2:
+                    print(Fore.BLUE + line)
+
+                elif LOGO_COLOR == 3:
+                    print(Fore.CYAN + line)
+
+                elif LOGO_COLOR == 4:
+                    print(Fore.GREEN + line)
+
+                elif LOGO_COLOR == 5:
+                    print(Fore.MAGENTA + line)
+
+                elif LOGO_COLOR == 6:
+                    print(Fore.YELLOW + line)
+
+                elif LOGO_COLOR == 0:
+                    print(Fore.WHITE + line)
+
+                else:
+                    log_error('Неверное значение для HEADER_COLOR в "theme.py", ' + \
+                              'значение должно быть в диапазоне от 0 до 6.')
+
+                    try:
+                        input()
+
+                    except:
+                        pass
+
+                    _exit(1)
+
             index += 1
 
     else:
-        lines = intro_text_mini.split('\n')
+        if LOGO_STYLE == 1:
+            lines = IntroText_Default_Mini.split('\n')
+
+        elif LOGO_STYLE == 2:
+            lines = IntroText_Bloody_Mini.split('\n')
+
+        elif LOGO_STYLE == 3:
+            lines = IntroText_Colossal_Mini.split('\n')
+
+        elif LOGO_STYLE == 4:
+            lines = IntroText_DOSRebel_Mini.split('\n')
+
+        else:
+            log_error('Неверное значение для HEADER_STYLE в "theme.py", ' + \
+                              'значение должно быть в диапазоне от 1 до 4.')
+
+            try:
+                input()
+
+            except:
+                pass
+
+            _exit(1)
+
         index = 0
 
         for line in lines:
-            print(get_color(colors[index], 'fg') + line)
+            if LOGO_COLOR == 1:
+                print(get_color(colors[index], 'fg') + line)
+
+            else:
+                if LOGO_COLOR == 2:
+                    print(Fore.BLUE + line)
+
+                elif LOGO_COLOR == 3:
+                    print(Fore.CYAN + line)
+
+                elif LOGO_COLOR == 4:
+                    print(Fore.GREEN + line)
+
+                elif LOGO_COLOR == 5:
+                    print(Fore.MAGENTA + line)
+
+                elif LOGO_COLOR == 6:
+                    print(Fore.YELLOW + line)
+
+                elif LOGO_COLOR == 0:
+                    print(Fore.WHITE + line)
+
+                else:
+                    log_error('Неверное значение для HEADER_COLOR в "theme.py", ' + \
+                              'значение должно быть в диапазоне от 0 до 6.')
+
+                    try:
+                        input()
+
+                    except:
+                        pass
+
+                    _exit(1)
+
             index += 1
--- Functions/logger.py
+++ Functions/logger.py
@@ -2,16 +2,12 @@ from datetime import datetime
 from colorama import Fore
 
 
-class logwriter:
-    written = ''
-
-
-def log(content, message_type='ИНФО', spaces=3, show_type=True):
+def log_error(content, message_type='ОШИБКА', spaces=1, show_type=True):
     if show_type:
         time_now = datetime.now().strftime('%H:%M:%S')
-        to_write = f'{Fore.GREEN}{time_now} {Fore.CYAN}[{message_type}]{" " * spaces}{Fore.GREEN}: {Fore.GREEN}{content}'
+        to_write = f'{Fore.RED}{time_now} [{message_type}]{" " * spaces}: {content}'
     else:
-        to_write = Fore.GREEN + content
+        to_write = Fore.RED + content
 
     print(to_write)
 
@@ -22,12 +18,61 @@ def log(content, message_type='ИНФО', spaces=3, show_type=True):
         logwriter.written = '\n'.join(logwriter.written[:50])
 
 
-def log_error(content, message_type='ОШИБКА', spaces=1, show_type=True):
+from os import _exit
+
+from theme import LOGS_TIME_AND_CONTENT_COLOR
+
+logs_time_and_content_color = Fore.GREEN if LOGS_TIME_AND_CONTENT_COLOR == 1 else (
+    Fore.BLUE if LOGS_TIME_AND_CONTENT_COLOR == 2 else
+    (Fore.CYAN if LOGS_TIME_AND_CONTENT_COLOR == 3 else
+     (Fore.MAGENTA if LOGS_TIME_AND_CONTENT_COLOR == 4 else
+      (Fore.YELLOW if LOGS_TIME_AND_CONTENT_COLOR == 5 else
+       (Fore.WHITE if LOGS_TIME_AND_CONTENT_COLOR == 0 else None)))))
+
+if not logs_time_and_content_color:
+    log_error('Неверное значение для LOGS_TIME_AND_CONTENT_COLOR в "theme.py", ' + \
+                              'значение должно быть в диапазоне от 0 до 5.')
+
+    try:
+        input()
+
+    except:
+        pass
+
+    _exit(1)
+
+from theme import LOGS_TYPE_COLOR
+
+logs_type_color = Fore.CYAN if LOGS_TYPE_COLOR == 1 else (
+    Fore.BLUE if LOGS_TYPE_COLOR == 2 else
+    (Fore.GREEN if LOGS_TYPE_COLOR == 3 else
+     (Fore.MAGENTA if LOGS_TYPE_COLOR == 4 else
+      (Fore.YELLOW if LOGS_TYPE_COLOR == 5 else
+       (Fore.WHITE if LOGS_TYPE_COLOR == 0 else None)))))
+
+if not logs_type_color:
+    log_error('Неверное значение для LOGS_TYPE_COLOR в "theme.py", ' + \
+                              'значение должно быть в диапазоне от 0 до 5.')
+
+    try:
+        input()
+
+    except:
+        pass
+
+    _exit(1)
+
+
+class logwriter:
+    written = ''
+
+
+def log(content, message_type='ИНФО', spaces=3, show_type=True):
     if show_type:
         time_now = datetime.now().strftime('%H:%M:%S')
-        to_write = f'{Fore.RED}{time_now} [{message_type}]{" " * spaces}: {content}'
+        to_write = f'{logs_time_and_content_color}{time_now} {logs_type_color}[{message_type}]{" " * spaces}{logs_time_and_content_color}: {content}'
     else:
-        to_write = Fore.RED + content
+        to_write = Fore.GREEN + content
 
     print(to_write)
 
